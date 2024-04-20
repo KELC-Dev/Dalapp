0b77a606cde0df52703327b8112c08875696c76f
KELC-dev <15j21n@gmail.com>
2024-04-19 19:52:36 -0500

    base terminada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [receiverInfo,setReceiverInfo] = useState("");
+
+  const inChanged = (e:ChangeEvent<HTMLTextAreaElement>) => {
+    setReceiverInfo(e.target.value);
+  }
+
+  const process = () => {
+    var txt = receiverInfo;
+    txt = txt.split("\n").join(", ");
+    
+    var htmlRes = (document.getElementById("rpta") as HTMLInputElement)
+    htmlRes.value = txt;
+  }
 
   return (
     <div className="tot">
@@ -12,11 +25,14 @@ function App() {
       </nav>
       <main className='princ'>
         <div className="left">
-          
+          <div className='receiver'>
+            <textarea onChange={(e) => inChanged(e)} name="in" id="in" cols={30} rows={10}></textarea>
+          </div>
+          <button onClick={() => process()} className='GoBTN'>Go Go Go</button>
         </div>
         <div className="right">
           <div className="res">
-            <input type="text" name="rpta" id="rpta" />
+            <textarea  readOnly name="rpta" id="rpta" cols={20} rows={30}></textarea>
           </div>
         </div>
       </main>
